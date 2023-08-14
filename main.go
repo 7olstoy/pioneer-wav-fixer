12ff664999b70a7283e0cc92a5e27e7a4443596d
Dmitry Tolstoy <33700018+7olstoy@users.noreply.github.com>
2023-08-14 19:42:22 +0200

    Added search in subfolders

--- main.go
+++ main.go
@@ -22,23 +22,18 @@ func main() {
 		return
 	}
 
-	// Read and list all WAV files in the specified folder
-	files, err := os.ReadDir(*folderPtr)
-	if err != nil {
-		fmt.Println("Error reading folder:", err)
-		return
-	}
-
-	// Loop through the files in the folder
-	for _, file := range files {
-		if !file.IsDir() && filepath.Ext(file.Name()) == ".wav" {
-			filePath := filepath.Join(*folderPtr, file.Name())
-
+	// Recursively search for all WAV files in the specified folder
+	err := filepath.WalkDir(*folderPtr, func(path string, d os.DirEntry, err error) error {
+		if err != nil {
+			fmt.Println("Error reading directory:", err)
+			return err
+		}
+		if !d.IsDir() && filepath.Ext(d.Name()) == ".wav" {
 			// Open the WAV file in read-write mode
-			wavFile, err := os.OpenFile(filePath, os.O_RDWR, 0644)
+			wavFile, err := os.OpenFile(path, os.O_RDWR, 0644)
 			if err != nil {
 				fmt.Println("Error opening file:", err)
-				continue
+				return nil // Continue to next file
 			}
 			defer wavFile.Close()
 
@@ -47,29 +42,32 @@ func main() {
 			_, err = wavFile.ReadAt(buffer, 20) // Read starting from the 20th byte (0-indexed)
 			if err != nil {
 				fmt.Println("Error reading file:", err)
-				continue
+				return nil // Continue to next file
 			}
 
 			// Combine the values of the 20th and 21st bytes
 			value := (uint16(buffer[0]) << 8) | uint16(buffer[1])
 
-			// If the value is 0001 and not showing all, skip the file
-			if value == 0100 && !*showAllPtr {
-				continue
-			}
+			// Convert value to hex "0100"
+			newHexValue := []byte{1, 0}
 
-			// If overwrite flag is true, update the file with 01 00 value
-			if *overwritePtr {
-				_, err := wavFile.WriteAt([]byte{1, 0}, 20) // Write 0001 at the correct position
+			// If overwrite flag is true, update the file with "0100" value
+			if *overwritePtr && value != 256 {
+				_, err := wavFile.WriteAt(newHexValue, 20) // Write "0100" at the correct position
 				if err != nil {
 					fmt.Println("Error writing to file:", err)
-					continue
+					return nil // Continue to next file
 				}
-				fmt.Printf("Updated \"%s\"\n", file.Name())
-			} else {
-				// Print filename and combined value as a hexadecimal string with leading zeros
-				fmt.Printf("HEX: \"%04X\", file: \"%s\"\n", value, filePath)
+				fmt.Printf("Updated \"%s\"\n", path)
+			} else if *showAllPtr || value != 256 {
+				// Print value and full path of the filename as a hexadecimal string
+				fmt.Printf("HEX: \"%04X\", file: \"%s\"\n", value, path)
 			}
 		}
+		return nil
+	})
+
+	if err != nil {
+		fmt.Println("Error walking directory:", err)
 	}
 }
